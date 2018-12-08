27206940c36b38100f5c11d89c9495ad6a0f36dc
Rima Bouhal <abouhal2@illinois.edu>
2018-12-08 17:44:21 -0600

    added the opening page here!

---
+++ app/src/main/java/com/example/sadiaahmed/finalproj/OpeningPage.java
@@ -0,0 +1,29 @@
+package com.example.sadiaahmed.finalproj;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class OpeningPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_opening_page);
+
+        Button startButton = (Button) findViewById(R.id.startButton);
+        startButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMainPage();
+            }
+        });
+        }
+    public void openMainPage() {
+        Intent mainIntent = new Intent( OpeningPage.this, MainActivity.class);
+        startActivity(mainIntent);
+    }
+}
